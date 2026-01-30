c095f37621f7eb7ea767f6ee5ea0cb85e94faeac
Sameerxh <32047689+Sameerxh@users.noreply.github.com>
2026-01-30 18:59:03 +0000

    added files

---
+++ data.inc.php
@@ -0,0 +1,36 @@
+<?php
+  // info for post 1
+  $post1_postLink = "postlink1.php";
+  $post1_userName = "Leonie Kohler";
+  $post1_userLink = "userlink1.php";
+  $post1_date = "2/8/2017";
+  $post1_thumb = "8710320515.jpg";
+  $post1_title = "Ekklisia Agii Isidori Church";
+  $post1_reviewsNum = 15;
+  $post1_reviewsRating = 3;
+  $post1_excerpt = "At the end of the hot climb up to the top Lycabettus Hill you are greeted with the oasis that is the Ekklisia Agii Isidori church.";
+
+  // info for post 2
+  $post2_postLink = "postlink2.php";
+  $post2_userName = "Frantisek Wichterlova";
+  $post2_userLink = "userlink2.php";
+  $post2_date = "9/9/2017";
+  $post2_thumb = "8710247776.jpg";
+  $post2_title = "Santorini Sunset";
+  $post2_reviewsNum = 38;
+  $post2_reviewsRating = 5;
+  $post2_excerpt = "Every evening as the sun sets in Fira, it seems that everyone who is not drinking or eating is rushing with their camera to the most picturesque locations in order to capture that famous Aegean sunset.";
+
+  // info for post 3
+  $post3_postLink = "postlink3.php";
+  $post3_userName = "Edward Francis";
+  $post3_userLink = "userlink3.php";
+  $post3_date = "10/19/2019";
+  $post3_thumb = "8710289254.jpg";
+  $post3_title = "Looking towards Fira";
+  $post3_reviewsNum = 3;
+  $post3_reviewsRating = 2;
+  $post3_excerpt = "The steamer Mongolia, belonging to the Peninsular and Oriental Company, built of iron, of two thousand eight hundred tons burden, and five hundred horse-power, was due at eleven o'clock a.m. on Wednesday, the 9th of October, at Suez.";
+
+  
+?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">  
+</head>
+
+<body>
+    <!-- Page Content -->
+    <main>     
+                <!-- start post summaries -->
+                <div>
+
+                    <!-- replace each of these rows with a function call -->
+                     <!--first row--> 
+                  
+                    <!--second row-->
+                
+                    <!--third row-->
+                 
+
+                </div>  <!-- end post list -->
+           
+    </main>
+    
+</body>
+
+</html>
