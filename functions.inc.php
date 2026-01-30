a5e777027292eb10d5b1573cc6656de5fc6bdd36
Sameerxh <32047689+Sameerxh@users.noreply.github.com>
2026-01-30 19:06:20 +0000

    added code to functions

---
+++ functions.inc.php
@@ -0,0 +1,53 @@
+<?php
+include 'data.inc.php';
+include 'functions.inc.php';
+
+function generateLink($url, $label) {
+    echo "<a href=\"$url\">$label</a>";
+}
+
+function outputStars($rating) {
+    for ($i = 0; $i < $rating; $i++) {
+        echo "<img src=\"images/star-gold.png\" alt=\"gold star\">";
+    }
+
+    for ($i = $rating; $i < 5; $i++) {
+        echo "<img src=\"images/star-white.png\" alt=\"white star\">";
+    }
+}
+
+function outputPostRow(
+    $post_link,
+    $post_userName,
+    $post_userLink,
+    $post_date,
+    $post_thumb,
+    $post_title,
+    $post_reviewsNum,
+    $post_reviewsRating,
+    $post_excerpt
+) {
+    echo "<div class='post'>";
+
+    generateLink($post_link, "<img src='images/$post_thumb' alt='$post_title'>");
+
+    echo "<h2>$post_title</h2>";
+
+    echo "Posted by ";
+    generateLink($post_userLink, $post_userName);
+    echo " on $post_date";
+
+    echo "<br>";
+    outputStars($post_reviewsRating);
+    echo " $post_reviewsNum Reviews";
+
+    echo "<p>$post_excerpt</p>";
+
+    generateLink($post_link, "Read more");
+
+    echo "</div>";
+}
+
+
+?>
+
