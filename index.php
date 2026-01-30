5c4345d2831caab6c7fbc29b6be6bbb0fdb8ae2f
Sameerxh <32047689+Sameerxh@users.noreply.github.com>
2026-01-30 19:13:04 +0000

    edited index.php

--- index.php
+++ index.php
@@ -1,27 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">  
-</head>
+<?php
 
-<body>
-    <!-- Page Content -->
-    <main>     
-                <!-- start post summaries -->
-                <div>
+include 'data.inc.php';
+include 'functions.inc.php';
 
-                    <!-- replace each of these rows with a function call -->
-                     <!--first row--> 
-                  
-                    <!--second row-->
-                
-                    <!--third row-->
-                 
+outputPostRow(
+    $post1_postLink,
+    $post1_userName,
+    $post1_userLink,
+    $post1_date,
+    $post1_thumb,
+    $post1_title,
+    $post1_reviewsNum,
+    $post1_reviewsRating,
+    $post1_excerpt
+);
 
-                </div>  <!-- end post list -->
-           
-    </main>
-    
-</body>
+outputPostRow(
+    $post2_postLink,
+    $post2_userName,
+    $post2_userLink,
+    $post2_date,
+    $post2_thumb,
+    $post2_title,
+    $post2_reviewsNum,
+    $post2_reviewsRating,
+    $post2_excerpt
+);
 
-</html>
+outputPostRow(
+    $post3_postLink,
+    $post3_userName,
+    $post3_userLink,
+    $post3_date,
+    $post3_thumb,
+    $post3_title,
+    $post3_reviewsNum,
+    $post3_reviewsRating,
+    $post3_excerpt
+);
+
+?>
